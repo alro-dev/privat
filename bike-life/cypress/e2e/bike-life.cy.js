bf3b2e84de08ca94f687ff03dc968b075de178d4
alro-dev <alarogala@gmail.com>
2023-02-24 15:54:12 +0100

    bike life - menu test

--- bike-life/cypress/e2e/bike-life.cy.js
+++ bike-life/cypress/e2e/bike-life.cy.js
@@ -1,5 +1,22 @@
+import 'cypress-each'
+
+const menuItems = [
+    ['comp-ihkffuqo1label', 'Portfolio'],
+    ['comp-ihkffuqo2label', 'Videos'],
+    ['comp-ihkffuqo3label','Offer'],
+    ['comp-ihkffuqo4label', 'Contact']
+]
+
 describe('Home Page', () => {
-  it('passes', () => {
-    cy.visit('https://alarogala.wixsite.com/bikelife')
+  it('page is loaded', () => {
+    var addressPage = 'https://alarogala.wixsite.com/bikelife'
+    cy.visit(addressPage);
+    cy.url().should('include', addressPage);
+    });
+
+    it.each(menuItems)('%s page has %s header', (id, text) => {
+      var addressPage = 'https://alarogala.wixsite.com/bikelife';
+      cy.visit(addressPage);
+      cy.get("#" + id).click(); //"#comp-ihkffuqo1label"
+    })
   })
-})
